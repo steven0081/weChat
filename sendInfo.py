6f00e9341a216fa33555f5c92d61523dbbfd02c1
Administrator <405886056@qq.com>
2019-07-03 21:04:39 +0800

    modify

--- sendInfo.py
+++ sendInfo.py
@@ -7,7 +7,7 @@ itchat.auto_login(hotReload=True)
 result = itchat.search_friends(name='董黎')
 user_name = result[0]['UserName']
 
-path= 'I:\\PycharmProjects\\untitled\\mzitu'
+path= 'I:\\PycharmProjects\\paChong\\mzitu'
 file_list = os.listdir(path)
 print(len(file_list))
 for f in file_list:
