dd4026d57a9a4f18f16ad2050644b6c187c35eaf
Administrator <405886056@qq.com>
2019-07-03 20:37:08 +0800

    Initial commit

---
+++ sendInfo.py
@@ -0,0 +1,19 @@
+import itchat
+import time
+import os
+
+itchat.auto_login(hotReload=True)
+
+result = itchat.search_friends(name='董黎')
+user_name = result[0]['UserName']
+
+path= 'I:\\PycharmProjects\\untitled\\mzitu'
+file_list = os.listdir(path)
+print(len(file_list))
+for f in file_list:
+    itchat.send_image(path+'\\'+f, user_name)
+    time.sleep(1)
+
+
+
+
